f1db34f4086fea31b74f236e28ce6b329b73383e
NKayz <kachenyasha@gmail.com>
2023-02-23 04:33:08 -0800

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point, print fizzbuzz integers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 101; i++)
+	{
+		if ((i % 3) == 0)
+			printf("Fizz");
+		if ((i % 5) == 0)
+			printf("Buzz");
+		if (((i % 3) != 0) && ((i % 5) != 0))
+			printf("%d", i);
+		if (i != 100)
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
